457be7f187848c19c8bbcd5939bc4bed588b4f9f
Letder40 <adrian04r@gmail.com>
2025-04-11 08:37:18 +0200

    fix: .gitignore bad name for config.mk

--- letos/kernel/arch/i386/hardware/keyboard/keyboard.c
+++ letos/kernel/arch/i386/hardware/keyboard/keyboard.c
@@ -9,10 +9,11 @@ volatile uint8_t input_index;
 
 void keyboard_handler() {
     uint8_t scancode = inb(0x60);
-    if (input_index < 255) {
-        input_buffer[input_index++] = scancode;
+    if (input_index == 255) {
+        input_index = 0;
     }
 
+    input_buffer[input_index++] = scancode;
     printf("key_pressed\n");
 }
 
